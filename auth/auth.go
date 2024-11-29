877c853b19ef74e675d033a966decd06d7678170
ball0803 <hackball08@gmail.com>
2024-11-29 17:01:01 +0700

    data encrytion aes-256 and log env for prod

--- auth/auth.go
+++ auth/auth.go
@@ -3,14 +3,22 @@ package auth
 import (
 	"alumni_api/config"
 	"alumni_api/models"
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"encoding/base64"
+	"errors"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
 	"golang.org/x/crypto/bcrypt"
+	"io"
 )
 
 var jwtSecret = []byte(config.GetEnv("JWT_SECRET_KEY", "secret_key"))
+var encryptionKey = []byte(config.GetEnv("ENCRYPTION_KEY", "default_32_byte_key")) // Must be 32 bytes for AES-256
 
+// GenerateJWT generates a JWT for a user
 func GenerateJWT(userID, role string, admitYear int) (string, error) {
 	claims := models.Claims{
 		UserID:    userID,
@@ -26,6 +34,7 @@ func GenerateJWT(userID, role string, admitYear int) (string, error) {
 	return token.SignedString(jwtSecret)
 }
 
+// ParseJWT validates and parses a JWT
 func ParseJWT(tokenString string) (*models.Claims, error) {
 	claims := &models.Claims{}
 	token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
@@ -39,12 +48,67 @@ func ParseJWT(tokenString string) (*models.Claims, error) {
 	return claims, nil
 }
 
+// HashPassword hashes a plain password
 func HashPassword(password string) (string, error) {
 	bytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	return string(bytes), err
 }
 
+// CheckPasswordHash compares a password with its hashed version
 func CheckPasswordHash(password, hash string) error {
 	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
 	return err
+}
+
+// Encrypt encrypts plaintext using AES-256 GCM
+func Encrypt(plaintext string) (string, error) {
+	block, err := aes.NewCipher(encryptionKey)
+	if err != nil {
+		return "", err
+	}
+
+	aesGCM, err := cipher.NewGCM(block)
+	if err != nil {
+		return "", err
+	}
+
+	nonce := make([]byte, aesGCM.NonceSize())
+	_, err = io.ReadFull(rand.Reader, nonce)
+	if err != nil {
+		return "", err
+	}
+
+	ciphertext := aesGCM.Seal(nonce, nonce, []byte(plaintext), nil)
+	return base64.StdEncoding.EncodeToString(ciphertext), nil
+}
+
+// Decrypt decrypts ciphertext using AES-256 GCM
+func Decrypt(ciphertext string) (string, error) {
+	decodedCiphertext, err := base64.StdEncoding.DecodeString(ciphertext)
+	if err != nil {
+		return "", err
+	}
+
+	block, err := aes.NewCipher(encryptionKey)
+	if err != nil {
+		return "", err
+	}
+
+	aesGCM, err := cipher.NewGCM(block)
+	if err != nil {
+		return "", err
+	}
+
+	nonceSize := aesGCM.NonceSize()
+	if len(decodedCiphertext) < nonceSize {
+		return "", errors.New("invalid ciphertext")
+	}
+
+	nonce, encryptedData := decodedCiphertext[:nonceSize], decodedCiphertext[nonceSize:]
+	plaintext, err := aesGCM.Open(nil, nonce, encryptedData, nil)
+	if err != nil {
+		return "", err
+	}
+
+	return string(plaintext), nil
 }
--- logger/logger.go
+++ logger/logger.go
@@ -2,15 +2,29 @@ package logger
 
 import (
 	"go.uber.org/zap"
+	"os"
 )
 
 func NewLogger() (*zap.Logger, error) {
-	config := zap.Config{
-		Encoding:         "console", // Switch to "console" for more readable output
-		EncoderConfig:    zap.NewDevelopmentEncoderConfig(),
-		OutputPaths:      []string{"stdout"},
-		ErrorOutputPaths: []string{"stderr"},
-		Level:            zap.NewAtomicLevelAt(zap.DebugLevel),
+	var config zap.Config
+
+	if os.Getenv("ENV") == "dev" {
+		config = zap.Config{
+			Encoding:         "console",
+			EncoderConfig:    zap.NewDevelopmentEncoderConfig(),
+			OutputPaths:      []string{"stdout"},
+			ErrorOutputPaths: []string{"stderr"},
+			Level:            zap.NewAtomicLevelAt(zap.DebugLevel),
+		}
+	} else {
+		config = zap.Config{
+			Encoding:         "json",
+			EncoderConfig:    zap.NewProductionEncoderConfig(),
+			OutputPaths:      []string{"stdout"},
+			ErrorOutputPaths: []string{"stderr"},
+			Level:            zap.NewAtomicLevelAt(zap.InfoLevel),
+		}
 	}
+
 	return config.Build()
 }
