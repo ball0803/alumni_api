961813dd19193fa32361e8fccbdbf5a26fb59c7c
ball0803 <hackball08@gmail.com>
2025-03-26 20:54:14 +0700

    Fix post view statistic

--- internal/repositories/statistic.go
+++ internal/repositories/statistic.go
@@ -18,50 +18,40 @@ func GetPostStat(ctx context.Context, driver neo4j.DriverWithContext, logger *za
 
 	query := `
     MATCH (p:Post)<-[:HAS_POST]-(author:UserProfile)
-    OPTIONAL MATCH (p)<-[l:LIKES]-(like_user:UserProfile)
+
     OPTIONAL MATCH (p)<-[v:HAS_VIEWED]-(view_user:UserProfile)
-    OPTIONAL MATCH (p)<-[c:COMMENTED_ON]-(comment_user:Comment)
+    WITH p, author, collect(view_user.generation) AS view_gens, collect(DISTINCT view_user.generation) AS view_gen_unique
+    WITH p, author, view_gens, view_gen_unique
 
-    WITH
-      p,
-      author
-      like_user.generation AS like_user_gen,
-      view_user.generation AS view_user_gen,
-      comment_user.generation AS comment_user_gen
+    OPTIONAL MATCH (p)<-[l:LIKES]-(like_user:UserProfile)
+    WITH p, author, view_gens, view_gen_unique, collect(like_user.generation) AS like_gens, collect(DISTINCT like_user.generation) AS like_gen_unique
 
-    WITH
-      p,
-      author
-      collect(like_user_gen) AS like_user_gens,
-      collect(DISTINCT like_user_gen) AS like_user_gen_unique,
-      collect(view_user_gen) AS view_user_gens,
-      collect(DISTINCT view_user_gen) AS view_user_gen_unique,
-      collect(comment_user_gen) AS comment_user_gens,
-      collect(DISTINCT comment_user_gen) AS comment_user_gen_unique,
+    OPTIONAL MATCH (p)<-[:COMMENTED_ON]-(:Comment)-[:COMMENTED_BY]->(comment_user:UserProfile)
+    WITH p, author, view_gens, view_gen_unique, like_gens, like_gen_unique, collect(comment_user.generation) AS comment_gens, collect(DISTINCT comment_user.generation) AS comment_gen_unique
 
     RETURN
-      p.post_id AS post_id,
+      p.post_id,
       p.title AS title,
       p.post_type AS post_type,
       p.media_urls AS media_urls,
       author.first_name + " " + author.last_name AS name,
       author.user_id AS user_id,
       author.profile_picture AS profile_picture,
-      size(like_user) AS like_count,
-      size(comment_user) AS comment_count,
-      size(view_user) AS view_count,
+      size(view_gens) AS view_count,
+      size(like_gens) AS like_count,
+      size(comment_gens) AS comment_count,
       {
-          key: like_user_gen_unique, 
-          value: [gen IN like_user_gen_unique | size([x IN like_user_gens WHERE x = gen])]
-      } AS like_user_gen,
-      {
-          key: view_user_gen_unique, 
-          value: [gen IN view_user_gen_unique | size([x IN view_user_gens WHERE x = gen])]
+          key: view_gen_unique, 
+          value: [gen IN view_gen_unique | size([x IN view_gens WHERE x = gen])]
       } AS view_user_gen,
       {
-          key: comment_user_gen_unique, 
-          value: [gen IN comment_user_gen_unique | size([x IN comment_user_gens WHERE x = gen])]
-      } AS comment_user_gen,
+          key: like_gen_unique, 
+          value: [gen IN like_gen_unique | size([x IN like_gens WHERE x = gen])]
+      } AS like_user_gen,
+      {
+          key: comment_gen_unique, 
+          value: [gen IN comment_gen_unique | size([x IN comment_gens WHERE x = gen])]
+      } AS comment_user_gen
   `
 
 	// Run the query
